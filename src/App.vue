db93ebdc423bf0d9a1c7b11a4652eb2c48000b89
RealdoBeja98 <realdo.beja@mail.polimi.it>
2022-12-07 22:38:56 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>  
+    <Navbar />
+    <section class="section">
+      <router-view/>
+    </section>
+  </div>
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  import Navbar from '@/components/layout/Navbar'
+  export default{
+    name:'App',
+    components : {
+      Navbar
     }
+
   }
-}
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma';
 </style>
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,30 @@
+<!--template tag is the html code that goes for this component-->
+<template>
+    <nav class="navbar is-dark">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item">
+                <strong>CRM VUE</strong>
+            </router-link>
+        </div>
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                            <router-link to="/sign-up" class="button is-success">
+                                <strong>Sign Up</strong>
+                            </router-link>
+                            <router-link to="/log-in" class="button is-light">
+                                Log In
+                            </router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default{
+        name: 'Nav-bar'
+    }
+</script>
